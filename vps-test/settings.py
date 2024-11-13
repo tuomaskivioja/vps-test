013865d5dea9729ca2f0db2641c362359949a889
Tuomas <tuomas@internetmadecoder.com>
2024-11-13 09:34:51 +0400

    a

--- vps-test/settings.py
+++ vps-test/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'vps-test.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'vps-test.wsgi.application'
 
 
 # Database
