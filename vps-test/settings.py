fb50c1141c1bb7fd3a0c1d08cec2a60620972206
Tuomas <tuomas@internetmadecoder.com>
2024-11-13 10:09:10 +0400

    a

--- vps-test/settings.py
+++ vps-test/settings.py
@@ -118,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+
+print(STATIC_ROOT)
+print(BASE_DIR)
 
 # Add the path to your React build files
 STATICFILES_DIRS = [
